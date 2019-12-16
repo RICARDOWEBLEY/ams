bd6b37f8edf94ea80decfb02c3f7f8dae30c85fd
RICARDOWEBLEY <56036998+RICARDOWEBLEY@users.noreply.github.com>
2019-12-15 21:47:10 -0500

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,17 +1,17 @@
 <?php 
 #Local DB Connection
-$host = '127.0.0.1';
-$db = 'ams';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
+#$host = '127.0.0.1';
+#$db = 'ams';
+#$user = 'root';
+#pass = '';
+#$charset = 'utf8mb4';
 
 // Remote Databse
-#$host = 'remotemysql.com';
- #$db = 'ENjPwibVWg';
-#$user = 'ENjPwibVWg';
-#$pass = '58kXmtL4c1';
-#$charset = 'utf8mb4';
+$host = 'remotemysql.com';
+$db = 'cQ7zDabgX5';
+$user = 'cQ7zDabgX5';
+$pass = '7LVT8rJWLF';
+$charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
     try{
         $pdo = new PDO($dsn, $user, $pass);
